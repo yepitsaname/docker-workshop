e34ea8d643f73f8fcff0b4a1840b6c4aa15dc578
Sam-Cusi <scusi21@gmail.com>
2025-08-21 15:15:07 -0500

    postman

--- api/server.js
+++ api/server.js
@@ -2,8 +2,18 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
+require ('dotenv').config({ path: '../db/.env' })
+const knexfile = require('../db/knexfile.js')
+const knex = require('knex')(knexfile['development'])
+
 app.get('/',(req, res)=>{
   res.status(200).send('My API is up and running YO');
+});
+
+app.get('/authors', (req, res)=>{ 
+  knex('authors').select('*').then(authors=>{
+    res.status(200).json(authors).send()
+  })
 });
 
 app.listen(port,()=>{console.log(`Express server listening on localhost:${port}`)});
